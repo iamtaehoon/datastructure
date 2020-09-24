67b344c4d4c9ac0065d1886aace716e3dd55b623
Kwanghoon Choi <lazyswamp@gmail.com>
2020-09-24 10:22:16 +0900

    stack examples

--- intro/main1.c
+++ intro/main1.c
@@ -3,7 +3,24 @@
 
 int main(void)
 {
-  // do something
+  int input;
+
+  scanf("%d", &input);
   
-	return 0;
+  while ( input != -1 ) {  // -1 ==> 입력 끝
+    push( input );
+
+    scanf("%d", &input);
+  }
+
+  printf("\n");
+  
+  while ( ! is_empty() ) {  // 스택이 비어있지 않으면
+    int elem = pop();
+    printf("%d ", elem);
+  }
+  
+  printf("\n");
+  
+  return 0;
 }
